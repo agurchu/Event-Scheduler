058fd403bbc7c98152d80f9aef40992044809695
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2025-12-16 09:52:41 +0200

    pipeline

--- src/main/java/agurchu/codewithritmo/scheduler/PersonalEvent.java
+++ src/main/java/agurchu/codewithritmo/scheduler/PersonalEvent.java
@@ -1,6 +1,75 @@
 package agurchu.codewithritmo.scheduler;
 
 public class PersonalEvent extends Event {
-    // TODO: Initialize default values
+    private String eventName ;
+    private String eventDate;
+    private int duration;
+    private String priority =  "Low";;
+
+
+    public PersonalEvent( ) {
+        this.eventName = "Personal Event";
+        this.eventDate = "2025-01-01";
+        this.duration = 60;
+    }
+
+    public PersonalEvent(String eventName, String eventDate, int duration){
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.duration = duration;
+
+    }
+
+    public PersonalEvent(String eventName){
+
+        this.eventName = eventName;
+    }
+
+
+    @Override
+    public String getEventName() {
+        return  eventName;
+    }
+
+    @Override
+    public void setEventName(PersonalEvent eventName) {
+
+    }
+
+
+    @Override
+
+    public String getPriority(){
+        return priority;
+    }
+    //
+    @Override
+    public void setEventName(String newEventName) {
+        eventName = newEventName;
+    }
+
+    @Override
+    public int getDuration() {
+        return duration;
+    }
+    //
+    @Override
+    public String getEventDate() {
+        return eventDate;
+    }
+    //
+    @Override
+    public void setEventDate(String newEventDate) {
+        this.eventDate = newEventDate;
+
+    }
+
+
+    @Override
+    public void setDuration(int newDuration) {
+        this.duration = newDuration;
+
+    }
+
 }
 
--- src/main/java/agurchu/codewithritmo/scheduler/WorkEvent.java
+++ src/main/java/agurchu/codewithritmo/scheduler/WorkEvent.java
@@ -2,5 +2,62 @@ package agurchu.codewithritmo.scheduler;
 
 
 public class WorkEvent extends Event {
-    // TODO: Initialize default values
+    private String eventName;
+    private String eventDate;
+    private int duration;
+    private String priority = "High";
+
+
+    public WorkEvent(){
+        this.eventName = "Work Event";
+        this.eventDate = "2025-01-01";
+        this.duration = 120;
+    }
+
+    public WorkEvent(String eventName, String eventDate, int duration){
+
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.duration = duration;
+
+    }
+    @Override
+    public String getEventName() {
+        return eventName;
+    }
+
+    @Override
+    public void setEventName(String updatedEventName) {
+        this.eventName = updatedEventName;
+    }
+
+    @Override
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    @Override
+    public void setEventDate(String updatedEventDate) {
+        this.eventDate = updatedEventDate;
+    }
+
+    @Override
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public void setDuration(int updatedDuration) {
+        this.duration = updatedDuration;
+    }
+
+    @Override
+    public void setEventName(PersonalEvent eventName) {
+
+    }
+
+    @Override
+    public String getPriority() {
+        return priority;
+    }
 }
